c153ab2144e641e6d214ed21d9a505d0163f8ada
Ákos Balogh <akos.balogh87@gmail.com>
2022-05-29 23:29:33 +0200

    Create base design and basic functions.

---
+++ javascript.js
@@ -0,0 +1,32 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    if(b===0){
+        return "Error"
+    }
+    return a/b;
+}
+
+function operate(func, a,b){
+    return func(a,b);
+}
+
+const display = document.querySelector(".output");
+let displayValue = parseFloat(display.textContent);
+
+const digits = document.querySelectorAll(".digits > button");
+
+
+
+
+console.log(displayValue);

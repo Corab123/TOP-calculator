c9b38ac9cff8bc5469cfa7a7e99a303f17028973
Corab123 <103074484+Corab123@users.noreply.github.com>
2022-05-30 15:12:40 +0200

    Create first working version

--- javascript.js
+++ javascript.js
@@ -18,19 +18,118 @@ function divide(a,b){
 }
 
 function operate(func, a,b){
-    return func(a,b);
+    displayValue = window[func](a,b);
+    display.textContent = displayValue;
+    return displayValue;
 }
 
-const display = document.querySelector(".output");
-let displayValue = parseFloat(display.textContent);
+const display = document.getElementById("calculatedValue");
+const history = document.getElementById('history');
+let displayValue = 0;
+let numberOfOperations = 0;
 
-const digits = document.querySelectorAll(".digits > button");
 
-digits.forEach(element => {
-    element.addEventListener('click', a => { 
-        display.textContent+=element.textContent
-    })
+const digits = document.querySelectorAll(".digitButton > button");
+const clear = document.getElementById("clear");
+const equal = document.getElementById("equal");
+
+digits.forEach(digit => {digit.addEventListener('click', addDigit(digit))});
+
+function addDigit(digit) {
+    return function () {
+        if(display.textContent==='0'){
+        display.textContent = digit.textContent;
+        } else {
+        display.textContent+=digit.textContent;
+        };
+    }
+};
+
+clear.addEventListener('click', clear => {
+    displayValue = 0;
+    numberOfOperations=0;
+    display.textContent = '';
+    history.textContent='';
+});
+
+const operators = document.querySelectorAll(".operators > button");
+operators.forEach(element => {
+    element.addEventListener('click', x => {
+        if(display.textContent==='0'){
+            return;
+        }
+        displayValue = parseFloat(display.textContent);
+            numberOfOperations++;
+            
+            switch(element.id){
+                case 'add':
+                    lastOperand = "+";
+                    break;
+                case 'subtract': 
+                    lastOperand = "-";
+                    break;                
+                case 'multiply': 
+                    lastOperand = "*";
+                    break;
+                case 'divide': 
+                    lastOperand = "/";
+                    break;  
+            };
+            history.textContent += " " + displayValue + " " + lastOperand;
+            display.textContent = 0;   
+    });
 });
 
 
-console.log(displayValue);
+function naturalSplit(str){
+    let arr =[];
+    let split = str.split(/(\d+)/);
+    for(let i in split){
+        let s = split[i];
+        if(s!== "") {
+            arr.push(s);
+        }
+    }
+    return arr;
+}
+equal.addEventListener('click', runEqual);
+
+function runEqual(){
+    history.textContent += " " +display.textContent + " =";
+    let myArray = naturalSplit(history.textContent);
+    console.log(myArray);
+    evaluate(myArray);
+}
+
+function evaluate(myArray){
+    len = myArray.length;
+    let calculation=0;
+    let a = 0;
+    let b = 0;
+    let operand = '';
+
+    for(let i=1;i<len-1;i++){
+        if(i===1){
+            a = parseFloat(myArray[i]);
+        }
+        if(isNaN(parseFloat(myArray[i]))){
+            if(myArray[i]===' + '){
+                operand = 'add';
+            } else if(myArray[i]===' - '){
+                operand = 'subtract';
+            } else if(myArray[i]===' * '){
+                operand = 'multiply';
+            } else {
+                operand = 'divide';
+            }
+        }else {
+            b = parseFloat(myArray[i]);
+        }
+        if(i>2 && i%2===1){
+            calculation = operate(operand,a,b);
+            console.log(calculation);
+            a = calculation;
+        }
+    }
+    numberOfOperations = 0
+}
